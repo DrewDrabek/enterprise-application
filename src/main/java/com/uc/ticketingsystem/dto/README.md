ef34e0af6bdda949e5ac678c945a5bb45bd13fdb
Drew Drabek <drabekam@mail.uc.edu>
2025-03-15 11:25:58 -0400

    adding the interfaces and some notes about the dtos

--- src/main/java/com/uc/ticketingsystem/dto/README.md
+++ src/main/java/com/uc/ticketingsystem/dto/README.md
@@ -2,4 +2,6 @@
 
 "Holds data structures used to transfer data between layers of your application (e.g., between controllers and services). DTOs often have a different structure than your database entities."
 
-Need to do more research on this one
+It seems that this is used for selecting what data is sent to the frontend instead of just interacting directly with the entity 
+
+technically these are not needed but they are considered best practice for security and for decoupling
--- src/main/java/com/uc/ticketingsystem/service/TicketService.java
+++ src/main/java/com/uc/ticketingsystem/service/TicketService.java
@@ -4,17 +4,21 @@ import enterprise.uc.ticketingsystem.model.Ticket;
 
 import java.util.List;
 
-public interface TicketService  {
+public interface TicketService {
 
-        Ticket createTicket(Ticket ticket);
+        // the idea here is these are the contracts that define what can be done but now how they are done
 
-        Ticket getTicketById(Long id);
+        // The idea is you define what return type there will be and then the function signature (name and what it takes it)
 
-        List<Ticket> getAllTickets();
+        TicketDto createTicket(TicketDto ticketDto, String externalUserId); //these are expected to return a ticket dto class we will need to make these
 
-        List<Ticket> getActiveTickets();
+        List<TicketDto> getAllTickets();
 
-        Ticket updateTicketStatus(Long id, String status);
+        TicketDto getTicketById(Long id);
+
+        TicketDto updateTicket(Long id, TicketDto ticketDto); //for updating entire ticket
+
+        TicketDto updateTicketStatus(Long id, String status); //For just updating the status
 
         void deleteTicket(Long id);
-    }
+}
