4378cd05bd5fcd40453b79514e289241a6b525b9
jacobg72 <202006502+jacobg72@users.noreply.github.com>
2025-03-20 14:11:43 -0400

    Use Lombok for DTO object instead of creating explicit getters and setters

--- src/main/java/com/uc/ticketingsystem/dto/TicketDto.java
+++ src/main/java/com/uc/ticketingsystem/dto/TicketDto.java
@@ -1,11 +1,12 @@
 package com.uc.ticketingsystem.dto;
 
-
 // This is the simplest item for the DTO
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class TicketDto {
 
     private Long id;
@@ -24,57 +25,6 @@ public class TicketDto {
     private String status;
 
     private String creatorUserId;
-
-
-    // Getters and Setters - we must have these
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getCreatorUserId() {
-        return creatorUserId;
-    }
-
-    public void setCreatorUserId(String creatorUserId) {
-        this.creatorUserId = creatorUserId;
-    }
 }
 
 
