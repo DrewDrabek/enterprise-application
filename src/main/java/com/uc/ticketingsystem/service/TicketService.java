6d9b1cf0da67768497b94c6ed731c5d8bf5f2cff
jacobg72 <202006502+jacobg72@users.noreply.github.com>
2025-03-20 14:34:33 -0400

    Document service layer interfaces

--- src/main/java/com/uc/ticketingsystem/service/TicketService.java
+++ src/main/java/com/uc/ticketingsystem/service/TicketService.java
@@ -5,20 +5,50 @@ import com.uc.ticketingsystem.dto.TicketDto;
 import java.util.List;
 
 public interface TicketService {
-
         // the idea here is these are the contracts that define what can be done but now how they are done
 
         // The idea is you define what return type there will be and then the function signature (name and what it takes it)
 
-        TicketDto createTicket(TicketDto ticketDto, String externalUserId); //these are expected to return a ticket dto class we will need to make these
+        /**
+         * Create a new ticket
+         * @param ticketDto The data for the new ticket
+         * @param externalUserId The ID of the user creating the ticket
+         * @return The created ticket
+         */
+        TicketDto createTicket(TicketDto ticketDto, String externalUserId);
 
+        /**
+         * Get all tickets
+         * @return A list of all tickets
+         */
         List<TicketDto> getAllTickets();
 
+        /**
+         * Get a ticket by ID
+         * @param id The ID of the ticket to get
+         * @return The ticket with the given ID
+         */
         TicketDto getTicketById(Long id);
 
-        TicketDto updateTicket(Long id, TicketDto ticketDto); //for updating entire ticket
+        /**
+         * Update an existing ticket
+         * @param id The ID of the ticket to update
+         * @param ticketDto The new data for the ticket
+         * @return The updated ticket
+         */
+        TicketDto updateTicket(Long id, TicketDto ticketDto);
 
-        TicketDto updateTicketStatus(Long id, String status); //For just updating the status
+        /**
+         * Update the status of an existing ticket
+         * @param id The ID of the ticket to update
+         * @param status The new status for the ticket
+         * @return The updated ticket
+         */
+        TicketDto updateTicketStatus(Long id, String status);
 
+        /**
+         * Delete a ticket
+         * @param id The ID of the ticket to delete
+         */
         void deleteTicket(Long id);
 }
--- src/main/java/com/uc/ticketingsystem/service/UserService.java
+++ src/main/java/com/uc/ticketingsystem/service/UserService.java
@@ -1,9 +1,13 @@
-// src/main/java/com/uc/ticketingsystem/service/UserService.java
 package com.uc.ticketingsystem.service;
 
 import com.uc.ticketingsystem.model.User;
 
 public interface UserService {
+    /**
+     * Find a user by their external ID, or create a new user if no user with the specified ID exists
+     * @param externalUserId The ID of the user
+     * @return The found or newly created user
+     */
     User findOrCreateUser(String externalUserId);
     // We really might not even need this it really will just depend on how we manage users - something that we will need to talk about this weekend
 }
