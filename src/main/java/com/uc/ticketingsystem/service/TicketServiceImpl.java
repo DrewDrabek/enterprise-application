de7bd0981c7e285e37684681f8b58c4f26d4250c
andrewdrabek <drabekam@mail.uc.edu>
2025-03-26 13:50:02 -0400

    Adding in javadocs to the TicketServiceImpl

--- src/main/java/com/uc/ticketingsystem/service/TicketServiceImpl.java
+++ src/main/java/com/uc/ticketingsystem/service/TicketServiceImpl.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation of the TicketService interface.
+ * Provides business logic for managing ticket operations.
+ */
+
 @Service
 public class TicketServiceImpl implements TicketService {
 
@@ -19,6 +24,16 @@ public class TicketServiceImpl implements TicketService {
 
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * Creates a new ticket and associates it with a user.
+     * If the user does not exist, a new user is created.
+     *
+     * @param ticketDto      The TicketDto containing ticket details.
+     * @param externalUserId The external user ID of the ticket creator.
+     * @return The created TicketDto.
+     * WE MIGHT WANT TO CHANGE THIS BUT THIS WAS NEW USER HANDLEING - THIS IS NOT GOOD FOR PRODUCTION AS COULD BE TAKEN ADVANTAGE OF
+     */
 
     @Override
     public TicketDto createTicket(TicketDto ticketDto, String externalUserId) {
@@ -40,15 +55,36 @@ public class TicketServiceImpl implements TicketService {
         return convertToDto(ticket);
     }
 
+    /**
+     * Retrieves all tickets.
+     *
+     * @return A list of TicketDtos representing all tickets.
+     */
+
     @Override
     public List<TicketDto> getAllTickets() {
         return ticketRepository.findAll().stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
+    /**
+     * Retrieves a ticket by its ID.
+     *
+     * @param id The ID of the ticket.
+     * @return The TicketDto if found, or null if not found.
+     */
+
     @Override
     public TicketDto getTicketById(Long id) {
         return ticketRepository.findById(id).map(this::convertToDto).orElse(null);
     }
+
+    /**
+     * Updates an existing ticket.
+     *
+     * @param id        The ID of the ticket to update.
+     * @param ticketDto The TicketDto containing updated ticket details.
+     * @return The updated TicketDto if successful, or null if ticket not found.
+     */
 
     @Override
     public TicketDto updateTicket(Long id, TicketDto ticketDto) {
@@ -65,6 +101,14 @@ public class TicketServiceImpl implements TicketService {
         return convertToDto(ticketRepository.save(existingTicket));
     }
 
+    /**
+     * Updates the status of a ticket.
+     *
+     * @param id     The ID of the ticket to update.
+     * @param status The new status of the ticket.
+     * @return The updated TicketDto if successful, or null if ticket not found.
+     */
+
     @Override
     public TicketDto updateTicketStatus(Long id, String status) {
         Ticket existingTicket = ticketRepository.findById(id).orElse(null);
@@ -77,10 +121,23 @@ public class TicketServiceImpl implements TicketService {
         return convertToDto(ticketRepository.save(existingTicket));
     }
 
+    /**
+     * Deletes a ticket by its ID.
+     *
+     * @param id The ID of the ticket to delete.
+     */
+
     @Override
     public void deleteTicket(Long id) {
         ticketRepository.deleteById(id);
     }
+
+    /**
+     * Converts a Ticket entity to a TicketDto.
+     *
+     * @param ticket The Ticket entity to convert.
+     * @return The corresponding TicketDto.
+     */
 
     private TicketDto convertToDto(Ticket ticket) {
         TicketDto dto = new TicketDto();
