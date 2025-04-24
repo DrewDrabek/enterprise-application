e7014a5b1ee8c018f5375f59179ef9ab3eb3d5fd
Paul Connolly <connolpe@mail.uc.edu>
2025-04-24 18:42:18 -0400

    Fixed Authentication Problem

---
+++ src/main/java/com/uc/ticketingsystem/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.uc.ticketingsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated() // All requests need to be authenticated
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/", true)
+                );
+
+        return http.build();
+    }
+}
